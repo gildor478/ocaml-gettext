91af75c1fc4cb1dc4ceba3d2f4955697a7d5889f
Sylvain Le Gall <sylvain@le-gall.net>
2004-06-14 00:58:10 +0200

    SVN revision 264
    Ignore-this: 82f00163d1e50a6c467c56a09fe51f5e
    
    darcs-hash:20040613225810-9e32d-6e311756ac814b1ad86d88df16023a862897a3c5

---
+++ src-native/camlgettext_parser.mly
@@ -0,0 +1,102 @@
+%{
+
+open Camlgettext_types;;
+
+%}
+
+%token EOF
+%token NPLURALS
+%token SEMICOLON
+%token PLURAL
+%token EQUAL
+%token CHARSET
+%token QUESTION_MARK
+%token COLON
+%token OR
+%token AND
+%token EQ
+%token NEQ
+%token LE
+%token L
+%token GE
+%token G
+%token PLUS
+%token MINUS
+%token MUL
+%token DIV
+%token MOD
+%token NOT
+%token ID
+%token RPAREN
+%token LPAREN
+
+%token <string*string> FIELD_NAME
+%token <string> CONTENT_TYPE
+%token <string> PLURAL_FORMS
+%token <int>    NUMBER
+%token <string> STRING
+
+%right QUESTION_MARK
+%left OR
+%left AND
+%left EQ NEQ
+%left G L GE LE
+%left PLUS MINUS
+%left MUL DIV MOD
+%right NOT
+
+%type < (string * string) list > main
+%start main
+
+%type < int * ( int -> int ) > plural_forms 
+%start plural_forms
+
+%type < string * string > content_type
+%start content_type
+
+%%
+
+main:
+  lines EOF    {  $1 }
+;
+
+lines:
+  lines line   { $2 :: $1 }
+| line         { [$1] } 
+;
+
+line: 
+  FIELD_NAME   { let (a,b) = $1 in (a, b) }
+| CONTENT_TYPE { ("Content-Type", $1) }
+| PLURAL_FORMS { ("Plural-Forms", $1) }
+;
+
+plural_forms:
+  NPLURALS EQUAL NUMBER SEMICOLON PLURAL EQUAL expr SEMICOLON { ($3,$7) }
+;
+
+content_type:
+  STRING SEMICOLON CHARSET EQUAL STRING             { ($1,$5) }
+;
+
+expr:
+ ID                                   { fun x -> x  }
+| NUMBER                              { fun x -> $1 }
+| expr QUESTION_MARK expr COLON expr  { fun x -> if ($1 x) != 0 then ($3 x) else ($5 x) }
+| expr OR expr                        { fun x -> if ($1 x) != 0 then 1 else ($3 x) }
+| expr AND expr                       { fun x -> if ($1 x) != 0 then 0 else ($3 x) }
+| expr EQ expr                        { fun x -> if ($1 x)  = ($3 x) then 1 else 0 }
+| expr NEQ expr                       { fun x -> if ($1 x) != ($3 x) then 1 else 0 }
+| expr LE expr                        { fun x -> if ($1 x) <= ($3 x) then 1 else 0 }
+| expr L expr                         { fun x -> if ($1 x) <  ($3 x) then 1 else 0 }
+| expr GE expr                        { fun x -> if ($1 x) >= ($3 x) then 1 else 0 }
+| expr G expr                         { fun x -> if ($1 x) >  ($3 x) then 1 else 0 }
+| expr PLUS expr                      { fun x -> ($1 x) + ($3 x) }
+| expr MINUS expr                     { fun x -> ($1 x) - ($3 x) }
+| expr MUL expr                       { fun x -> ($1 x) * ($3 x) }
+| expr DIV expr                       { fun x -> ($1 x) / ($3 x) }
+| expr MOD expr                       { fun x -> ($1 x) mod ($3 x) }
+| NOT expr                            { fun x -> if ($2 x) = 0 then 1 else 0 }
+| LPAREN expr RPAREN                  { fun x -> $2 x}
+;
+%%
---
+++ src-native/camlgettext_types.ml
@@ -0,0 +1,106 @@
+type endianess = ArchEndian | NotArchEndian
+;;
+
+exception Bad_mo_file
+;;
+
+(* Specification of .MO file *)
+(*
+   From GNU Gettext documentation 
+   ( http://www.gnu.org/software/gettext/manual/html_mono/gettext.html#SEC136 ).
+
+   Format of MO file :
+
+        byte
+             +------------------------------------------+
+          0  | magic number = 0x950412de                |
+             |                                          |
+          4  | file format revision = 0                 |
+             |                                          |
+          8  | number of strings                        |  == N
+             |                                          |
+         12  | offset of table with original strings    |  == O
+             |                                          |
+         16  | offset of table with translation strings |  == T
+             |                                          |
+         20  | size of hashing table                    |  == S
+             |                                          |
+         24  | offset of hashing table                  |  == H
+             |                                          |
+             .                                          .
+             .    (possibly more entries later)         .
+             .                                          .
+             |                                          |
+          O  | length & offset 0th string  ----------------.
+      O + 8  | length & offset 1st string  ------------------.
+              ...                                    ...   | |
+O + ((N-1)*8)| length & offset (N-1)th string           |  | |
+             |                                          |  | |
+          T  | length & offset 0th translation  ---------------.
+      T + 8  | length & offset 1st translation  -----------------.
+              ...                                    ...   | | | |
+T + ((N-1)*8)| length & offset (N-1)th translation      |  | | | |
+             |                                          |  | | | |
+          H  | start hash table                         |  | | | |
+              ...                                    ...   | | | |
+  H + S * 4  | end hash table                           |  | | | |
+             |                                          |  | | | |
+             | NUL terminated 0th string  <----------------' | | |
+             |                                          |    | | |
+             | NUL terminated 1st string  <------------------' | |
+             |                                          |      | |
+              ...                                    ...       | |
+             |                                          |      | |
+             | NUL terminated 0th translation  <---------------' |
+             |                                          |        |
+             | NUL terminated 1st translation  <-----------------'
+             |                                          |
+              ...                                    ...
+             |                                          |
+             +------------------------------------------+
+
+*)
+
+type mo_header_type = {
+  endianess                : endianess;
+  file_format_revision     : int32;
+  number_of_strings        : int32;
+  offset_table_strings     : int32;
+  offset_table_translation : int32;
+  size_of_hashing_table    : int32;
+  offset_of_hashing_table  : int32;
+}
+;;
+
+(* Details associated with "" *)
+(* Project-Id-Version: PACKAGE VERSION\n        *)
+(* Report-Msgid-Bugs-To: \n                     *)
+(* POT-Creation-Date: 2004-05-31 16:53+0200\n   *)
+(* PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n    *)
+(* Last-Translator: FULL NAME <EMAIL@ADDRESS>\n *)
+(* Language-Team: LANGUAGE <LL@li.org>\n        *)
+(* MIME-Version: 1.0\n                          *)
+(* Content-Type: text/plain; charset=CHARSET\n  *)
+(* Content-Transfer-Encoding: 8bit\n            *)
+(* Plural-Forms: specific ( 0 is false and 1 is *)
+(* true                                         *)
+
+type mo_translation_type = {
+  translation_from_string   : (string, string) Hashtbl.t;
+  project_id_version        : string option;
+  report_msgid_bugs_to      : string option;
+  pot_creation_date         : string option;
+  po_revision_date          : string option;
+  last_translator           : string option;
+  language_tream            : string option;
+  mime_version              : string option;
+  content_type              : string option;
+  content_transfer_encoding : string option;
+  plural_forms              : string option;
+  (* The only interesting fields *)
+  (* Those field are precomputed for regular use *)
+  content_type_charset      : string;
+  nplurals                  : int;
+  fun_plural_forms          : int -> int; 
+}
+;;
