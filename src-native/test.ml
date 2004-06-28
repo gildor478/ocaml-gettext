f5aa57952986f7c5f313b85f2e7555e8b187fe63
Sylvain Le Gall <sylvain@le-gall.net>
2004-06-28 22:29:14 +0200

    Use the new find scheme of ocaml-fileutils -- SLG
    Ignore-this: 303d2f3f330144243b97c313be2ec624
    SVN revision 277
    
    darcs-hash:20040628202914-9e32d-4cb7806f19d5c197001b8b2a4e78ae2b4fe28213

--- src-native/test.ml
+++ src-native/test.ml
@@ -1,22 +1,11 @@
-
+open FileUtil;;
 open FileUtil.StrUtil;;
 open Camlgettext;;
 
-let file = ref []
-in
-let _ = 
-  Arg.parse 
-  [
-    ("-search", (Arg.String( fun s -> 
-        print_endline ("Searching "^s);
-        file := (find (Match(".*\\.mo")) s) @ !file )
-      ), "Search the directory for .mo file")
-  ] 
-  (fun str -> file := str :: !file )
-  "Camlgettext v0.2 Sylvain Le Gall"
-in
 let read_one_mo file = 
   try 
+    let _ = print_endline file 
+    in
     let mo_file = open_in_bin file
     in
     let header = input_mo_header mo_file
@@ -27,5 +16,14 @@ let read_one_mo file =
   with Camlgettext_types.Bad_mo_file ->
     print_endline "Bad mo file"
 in
-List.iter (fun s -> print_endline s; read_one_mo s) !file
+Arg.parse 
+[
+  ("-search", (Arg.String( fun s -> 
+      print_endline ("Searching "^s);
+      find (Has_extension "mo") s (fun () fln -> read_one_mo fln) ()
+      )
+    ), "Search the directory for .mo file")
+] 
+(fun str -> read_one_mo str )
+"Camlgettext v0.2 Sylvain Le Gall"
 ;;
