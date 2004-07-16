78b7382b8fbd7bd28afac6e66931956812569ab3
Sylvain Le Gall <sylvain@le-gall.net>
2004-07-17 00:51:34 +0200

    Some dummy module -- SLG
    Ignore-this: 3eeb7b705cfb96fde69da231c9f89dd8
    SVN revision 299
    
    darcs-hash:20040716225134-9e32d-78eef38b0d848c2c4a3c5df41bfbb8842d95a620

--- libgettext-ocaml/gettextCharset.ml
+++ libgettext-ocaml/gettextCharset.ml
@@ -3,6 +3,26 @@
 
 module type CHARSET_TYPE = 
   sig
+    type t
+    
+    (** create in_enc out_enc : create a new charset converter from charset 
+        in_enc to out_enc.
+    *)
+    val create : string -> string -> t
+
+    (** recode str enc : return a transcoded string according to enc.
+    *)
+    val recode : string -> t -> string
   end
 ;;
 
+module Dummy : CHARSET_TYPE =
+  struct
+
+    type t = ()
+
+    let create in_enc out_enc = ()
+
+    let recode str () = str
+  end
+;;
--- libgettext-ocaml/gettextTranslate.ml
+++ libgettext-ocaml/gettextTranslate.ml
@@ -1,6 +1,42 @@
 (** Signature of module for translation storage / access *)
 
 module type TRANSLATE_TYPE =
+  functor ( Charset : GettextCharset.CHARSET_TYPE ) ->
   sig
+    type t
+
+    (** create chn charset : create a translation table using chn as 
+        the file descriptor for a mo file and charset as the charset 
+        transcoder 
+    *)
+    val create : in_channel -> Charset.t -> t
+
+    (** translate str (plural_form,number) tbl : translate the string 
+        str using tbl. It is possible that the operation modify tbl, 
+        so it is returned also. It is also possible to get the plural 
+        form of the translated string using plural_form and number.
+    *)
+    val translate : 
+         string 
+      -> ?plural_form: (string * int)
+      -> t 
+      -> string * t
+  end
+;;
+
+module Dummy : TRANSLATE_TYPE =
+  functor ( Charset : GettextCharset.CHARSET_TYPE ) ->
+  struct
+    type t = Charset.t
+
+    let create chn charset = charset
+
+    let translate str ?plural_form charset = 
+      match plural_form with
+        None 
+      | Some(_,0) ->
+          ( Charset.recode str charset, charset )
+      | Some(str_plural,_) ->
+          ( Charset.recode str_plural charset, charset)
   end
 ;;
