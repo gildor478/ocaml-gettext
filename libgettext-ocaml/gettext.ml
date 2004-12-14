fb2c7f826d2b05203a478d45aa1d685234fdd18a
Sylvain Le Gall <sylvain@le-gall.net>
2004-12-14 23:46:13 +0100

    Remove of gettextGlobal : better use simply gettext -- SLG
    Ignore-this: 95a3fcb318b8cc4d1e0164dc5daea85d
    SVN revision 326
    
    darcs-hash:20041214224613-9e32d-9fc93c7fe263837421c2fe16f76a1cf953484e4e

--- libgettext-ocaml/gettext.ml
+++ libgettext-ocaml/gettext.ml
@@ -1,6 +1,47 @@
 open GettextTypes;;
 open GettextCompat;;
 
+(* Function the main global variable of gettext with/without thread *)
+
+type global_type = {
+  failsafe   : failsafe;
+  categories : category list;
+  codesets   : (textdomain * codeset) list;
+  dirs       : (textdomain * dir) list;
+  codeset    : codeset
+  language   : language;
+}
+;;
+
+let critical_section = ref ( fun x -> x () )
+;;
+
+let global = ref {
+  failsafe   = Ignore;
+  categories = [];
+  codesets   = [];
+  dirs       = [];
+  codeset    = codeset;
+  language   = language;
+}
+;;
+
+let get_global () = 
+  !critical_section ( 
+    fun () ->
+      !global
+  )
+;;
+
+let set_global glb = 
+  !critical_section (
+    fun () ->
+      global := glb
+  )
+;;
+
+let get_global_t' () =
+
 (* High level functions *)
 
 module Library =
