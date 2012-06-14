175bfc20967112a25937ce9baacef4a2c683b8d0
sylvain.le-gall <sylvain.le-gall@polytechnique.org>
2012-06-15 00:53:00 +0200

    Fix missing +x bit on configure for CI
    Ignore-this: 14e4bddbd01f11e7f0fc05e1026c2919
    
    darcs-hash:20120614225300-4d692-94becda56bbe2ba2039f0460426bb64f74739431

--- ci-main.lua
+++ ci-main.lua
@@ -21,6 +21,6 @@ godi.build("godi-camomile")
 godi.build("godi-ocaml-fileutils")
 godi.build("godi-ounit")
 
-ci.exec("./configure", "--disable-doc", "--enable-test")
+ci.exec("sh", "configure", "--disable-doc", "--enable-test")
 ci.exec("make", "all")
 ci.exec("make", "test")
