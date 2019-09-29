448bee79b91db39600e66b24154288564c409c89
Sylvain Le Gall <sylvain@le-gall.net>
2019-09-29 10:33:36 +0200

    Use discover-stub to find gettext library on the target system.

---
+++ src/tools/discover-stub/discover.ml
@@ -0,0 +1,37 @@
+module C = Configurator.V1
+
+let gettext_code = {|
+#include <libintl.h>
+
+int main() {
+  gettext("abcd");
+  return 0;
+}
+|}
+
+let () =
+  C.main ~name:"gettext" (fun c ->
+      let is_working (c_flags, link_flags) =
+        C.c_test c gettext_code ~link_flags ~c_flags
+      in
+      let c_flags, link_flags =
+        try
+          List.find
+            is_working
+            [
+              (* Default that should work on standard Linux distributions. *)
+              ([], []);
+
+              (* MacOS with Homebrew, the library is "keg-only" to prevent
+               * conflict with system installed BSD gettext library, so we have
+               * to pull it from /usr/local.
+               * https://formulae.brew.sh/formula/gettext
+               *)
+              (["-I/usr/local/opt/gettext/include"],
+               ["-L/usr/local/opt/gettext/lib"; "-lintl"]);
+            ]
+        with Not_found ->
+          C.die "no ways to compile with gettext library"
+      in
+      C.Flags.write_sexp "c_flags.sexp" c_flags;
+      C.Flags.write_sexp "c_library_flags.sexp" link_flags)
